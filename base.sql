9e1ff28481e027707c3f42253e58eb81bb96c4be
juztnelubka <justnelubpower@gmail.com>
2020-03-26 05:04:51 +0300

    commit

---
+++ base.sql
@@ -0,0 +1,110 @@
+drop database if exists paladins;
+create database paladins;
+use paladins;
+
+drop table if exists users;
+create table users(
+	id serial primary key,
+	nickname varchar(20),
+	email varchar(255) unique,
+	password_hash char(20),
+	is_banned boolean default false,
+    index (nickname),
+	index (email)
+);
+
+
+
+drop table if exists modes;
+create table modes(
+    id int unsigned not null primary key,
+    name varchar(20)
+);
+
+drop table if exists maps;
+create table maps(
+    id int unsigned not null primary key,
+    name varchar(25)
+);
+
+drop table if exists game_types;
+create table game_types(
+    id int unsigned not null primary key,
+    name varchar(20)
+);
+
+drop table if exists champions;
+create table champions(
+    id int unsigned not null primary key,
+    name varchar(20),
+    class enum('Front Line','Damage','Support','Flank')
+);
+
+drop table if exists matches;
+create table matches(
+    id serial primary key,
+    blue_win boolean,
+    duration time,
+    map_id int references maps(id),
+    mode_id int references modes(id),
+    game_type_id int references game_types(id),
+    chat text
+);
+
+drop table if exists users_matches;
+create table users_matches(
+  primary key (match_id,user_id),
+  match_id bigint references matches(id),
+  user_id bigint references users(id),
+  champion_id int references champions(id),
+  team enum('Blue','Red'),
+  kills tinyint,
+  deaths tinyint,
+  assists tinyint,
+  credits int,
+  objective_time int,
+  damage int,
+  shielding int,
+  healing int,
+  maximum_kill_streak int,
+  party_id int,
+  party_size int
+);
+
+
+drop table if exists reports;
+create table reports(
+    id serial primary key,
+    type enum('Cheating','Griefing','Intentional Feeding','Other','Good skill','Nice teammate','Leadership'),
+    initiator_id bigint references users(id),
+    target_id bigint references users(id),
+    match_id bigint references matches(id),
+    body text,
+    index(initiator_id),
+    index(target_id)
+);
+
+
+
+drop table if exists `profiles`;
+create table `profiles`(
+    user_id serial primary key,
+    photo_id bigint unsigned null,
+    created_at datetime default now(),
+    foreign key (user_id) references users(id)
+                       on update cascade
+                       on delete restrict
+);
+
+
+drop table if exists friend_requests;
+create table friend_requests (
+    primary key (initiator_user_id, target_user_id),
+	initiator_user_id bigint unsigned not null references users(id),
+    target_user_id bigint unsigned not null references users(id),
+    `status` enum('requested', 'approved', 'unfriended', 'declined'),
+	requested_at datetime default now(),
+	confirmed_at datetime,
+	index (initiator_user_id),
+    index (target_user_id)
+);
---
+++ values.sql
@@ -0,0 +1,28 @@
+use paladins;
+
+insert maps (id,name)
+    values(1,'Frog Isle'), (2,'Jaguar Falls'),(3,'Seprent Beach'),(4,'Frozen Guard'),(5,'Ice Mines'),(6,'Fish Market'),
+    (7,'Timber Mill'),(8,'Stone Keep'),(9,'Brightmarsh'),(10,'Splitstone Quarry'),(11,'Ascension Peak'),(12,"Warder's Gate"),(13,'Shattered Desert'),(14,'Bazaar'),
+    (15,'Primal Court'),(16,"Foreman's Rise"),(17,"Magistrate's Archivies"),(18,"Marauder's Port"),(19,'Snowfall Junction'),(20,'Trade District'),(21,'Abyss'),(22,'Throne'),(23,'Dragon Arena')
+;
+
+insert modes (id,name)
+    values(1,'Siege'),(2,'Onslaught'),(3,'KotH'),(4,'Team Deathmatch');
+
+insert champions(id,name,class)
+    values(1,'Ash','Front Line'),(2,'Atlas','Front Line'),(3,'Barik','Front Line'),(4,'Fernando','Front Line'),(5,'Inara','Front Line'),(6,'Khan','Front Line'),(7,'Makoa','Front Line'),(8,'Raum','Front Line'),
+           (9,'Ruckus','Front Line'),(10,'Terminus','Front Line'),(11,'Torvald','Front Line'),
+           (12,'Bomb King','Damage'),(13,'Cassie','Damage'),(14,'Dredge','Damage'),(15,'Drogoz','Damage'),(16,'Imani','Damage'),(17,'Kinessa','Damage'),(18,'Lian','Damage'),
+           (19,'Sha Lin','Damage'),(20,'Strix','Damage'),(21,'Tiberius','Damage'),(22,'Tyra','Damage'),(23,'Viktor','Damage'),(24,'Vivian','Damage'),(25,'Willo','Damage'),
+           (26,'Furia','Support'),(27,'Grohk','Support'),(28,'Grover','Support'),(29,'Io','Support'),(30,'Jenos','Support'),(31,"Mal'Damba",'Support'),(32,'Pip','Support'),(33,'Seris','Support'),
+           (34,'Ying','Support'),
+           (35,'Androxus','Flank'),(36,'Buck','Flank'),(37,'Evie','Flank'),(38,'Koga','Flank'),(39,'Lex','Flank'),(40,'Maeve','Flank'),(41,'Moji','Flank'),(42,'Skye','Flank'),
+           (43,'Talus','Flank'),(44,'Zhin','Flank')
+           ;
+
+insert game_types(id,name)
+    values(1,'Casual'),(2,'Ranked'),(3,'Custom');
+
+
+
+
